ae05298b13f6e70c51c550536bc6eab2d9652585
Andrés Mata <andreseduardomp@gmail.com>
2018-01-14 19:53:41 +0100

    Implementado el login de Spotify, a la espera de controlar datos importantes con un Web API Wrapper

--- app/src/main/java/com/anedma/nightspot/activities/LoginActivity.java
+++ app/src/main/java/com/anedma/nightspot/activities/LoginActivity.java
@@ -2,8 +2,8 @@ package com.anedma.nightspot.activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
@@ -16,11 +16,20 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationHandler;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 public class LoginActivity extends Activity implements View.OnClickListener {
 
-    private static final int RC_SIGN_IN = 123; //Este es el código de vuelta que usara la API para saber que el usuario ha conseguido loguearse.
-    GoogleSignInClient mGoogleSignIn;
+    private static final int REQUESTCODE_GOOGLE_SIGNIN = 123; //Este es el código de vuelta que usara la API para saber que el usuario ha conseguido loguearse.
+    private static final int REQUESTCODE_SPOTIFY_SIGNIN = 1337;
+    private static final String SPOTIFY_CLIENT_ID = "d075f2e5efcc4be6b5cb879a49950de5";
+    private static final String REDIRECT_SPOTIFY_URI = "nightspot://callback";
+    private static final String[] SCOPES = {"playlist-read-private", "playlist-read-collaborative", "user-follow-read", "user-library-read", "user-top-read"};
+    private GoogleSignInClient mGoogleSignIn;
+    private AuthenticationRequest.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +37,13 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.activity_login);
         setupUI();
 
+        //Google Login
         GoogleSignInOptions mGoogleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         mGoogleSignIn = GoogleSignIn.getClient(this, mGoogleSignInOptions);
+
+        //Spotify Login
+        builder = new AuthenticationRequest.Builder(SPOTIFY_CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_SPOTIFY_URI);
+        builder.setScopes(SCOPES);
     }
 
     private void setupUI() {
@@ -45,6 +59,7 @@ public class LoginActivity extends Activity implements View.OnClickListener {
         if(account != null) {
             //TODO: Gestionar la entrada del usuario con Google
             //El usuario ya se ha logueado previamente, puede acceder.
+            Log.d("GOOGLE", "LOGIN SATISFACTORIO");
         }
         super.onStart();
     }
@@ -52,11 +67,33 @@ public class LoginActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        Log.d("LOGIN", "Se ha devuelto el resultado de una petición: " + requestCode);
         //Si el resultado de la petición es correcto
-        if(resultCode == RC_SIGN_IN) {
+        if(requestCode == REQUESTCODE_GOOGLE_SIGNIN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleGoogleSignInResult(task);
+        } else if (requestCode == REQUESTCODE_SPOTIFY_SIGNIN) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, data);
+            switch (response.getType()) {
+                case TOKEN:
+                    //TODO: Login satisfactorio para Spotify
+                    Log.d("SPOTIFY", "LOGIN SATISFACTORIO");
+                    String accessToken = AuthenticationClient.getResponse(resultCode, getIntent()).getAccessToken();
+                    break;
+                case ERROR:
+                    //TODO: Login erroneo para Spotify
+                    Log.d("SPOTIFY", "LOGIN ERRONEO");
+                    Toast.makeText(this, "Error al iniciar sesión con Spotify", Toast.LENGTH_LONG).show();
+                    break;
+                default:
+            }
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        AuthenticationClient.stopLoginActivity(this, REQUESTCODE_SPOTIFY_SIGNIN);
     }
 
     private void handleGoogleSignInResult(Task<GoogleSignInAccount> completedTask) {
@@ -64,9 +101,12 @@ public class LoginActivity extends Activity implements View.OnClickListener {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
             //TODO: Gestionar la entrada del usuario con Google
             //Si esto no retorna un fallo, el usuario se ha logueado correctamente, ya puede acceder
+            Log.d("GOOGLE", "LOGIN SATISFACTORIO");
         } catch (ApiException e) {
             //Si hay un error, la API devolverá un resultado según corresponda.
             Log.w("GOOGLESIGNIN", "El Login con Google ha fallado con el código: " + e.getStatusCode());
+            Log.d("GOOGLE", "LOGIN ERRONEO");
+            Toast.makeText(this, "Error al iniciar sesión con Google", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -77,7 +117,7 @@ public class LoginActivity extends Activity implements View.OnClickListener {
                 loginWithGoogle();
                 break;
             case R.id.spotifySignInButton:
-                Toast.makeText(this, "TEST LOGIN SPOTIFY", Toast.LENGTH_SHORT).show();
+                loginWIthSpotify();
                 break;
             default:
                     break;
@@ -86,6 +126,11 @@ public class LoginActivity extends Activity implements View.OnClickListener {
 
     private void loginWithGoogle() {
         Intent signInIntent = mGoogleSignIn.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+        startActivityForResult(signInIntent, REQUESTCODE_GOOGLE_SIGNIN);
+    }
+
+    private void loginWIthSpotify() {
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUESTCODE_SPOTIFY_SIGNIN, request);
     }
 }
